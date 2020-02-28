b9311282c3dd57b47cd89d0dfd63b1713a81d83d
高庆伟 <gaoqingwei@goldwind.com.cn>
2020-02-28 18:24:38 +0800

    测试提交

---
+++ src/test/java/com/mxs/wxproject/WxprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mxs.wxproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WxprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
